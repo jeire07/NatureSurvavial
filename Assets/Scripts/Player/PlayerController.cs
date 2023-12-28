321f5b29fb40730cffcc691602755cacac4db9d8
jeire07 <149458625+jeire07@users.noreply.github.com>
2023-12-28 13:52:54 +0900

    [feat] player control
    added actions
    - move
    - look
    - jump

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,143 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController Instance;
+
+    [Header("Movement")]
+    public LayerMask GroundLayerMask;
+    public float MoveSpeed;
+    public float JumpForce;
+    private Vector2 CurMovementInput;
+
+    [Header("Look")]
+    public Transform CameraContainer;
+    public float MinXLook;
+    public float MaxXLook;
+    public float LookSensitivity;
+    private float _camCurXRot;
+
+    private Vector2 _mouseDelta;
+
+    [HideInInspector]
+    public bool CanLook = true;
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(Instance);
+        }
+
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void LateUpdate()
+    {
+        if (CanLook)
+        {
+            CameraLook();
+        }
+    }
+
+    private void Move()
+    {
+        Vector3 dir = transform.forward * CurMovementInput.y + transform.right * CurMovementInput.x;
+        dir *= MoveSpeed;
+        dir.y = _rigidbody.velocity.y;
+
+        _rigidbody.velocity = dir;
+    }
+
+    void CameraLook()
+    {
+        _camCurXRot += _mouseDelta.y * LookSensitivity;
+        _camCurXRot = Mathf.Clamp(_camCurXRot, MinXLook, MaxXLook);
+        CameraContainer.localEulerAngles = new Vector3(-_camCurXRot, 0, 0);
+
+        transform.eulerAngles += new Vector3(0, _mouseDelta.x * LookSensitivity, 0);
+    }
+
+    public void OnLookInput(InputAction.CallbackContext context)
+    {
+        _mouseDelta = context.ReadValue<Vector2>();
+    }
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            CurMovementInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            CurMovementInput = Vector2.zero;
+        }
+    }
+
+    public void OnJumpInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            if (IsGrounded())
+                _rigidbody.AddForce(Vector2.up * JumpForce, ForceMode.Impulse);
+
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        Ray[] rays = new Ray[4]
+        {
+            new Ray(transform.position + (transform.forward * 0.2f) + (Vector3.up * 0.01f) , Vector3.down),
+            new Ray(transform.position + (-transform.forward * 0.2f)+ (Vector3.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (transform.right * 0.2f) + (Vector3.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.right * 0.2f) + (Vector3.up * 0.01f), Vector3.down),
+        };
+
+        for (int i = 0; i < rays.Length; i++)
+        {
+            if (Physics.Raycast(rays[i], 0.1f, GroundLayerMask))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position + (transform.forward * 0.2f), Vector3.down);
+        Gizmos.DrawRay(transform.position + (-transform.forward * 0.2f), Vector3.down);
+        Gizmos.DrawRay(transform.position + (transform.right * 0.2f), Vector3.down);
+        Gizmos.DrawRay(transform.position + (-transform.right * 0.2f), Vector3.down);
+    }
+
+    public void ToggleCursor(bool toggle)
+    {
+        Cursor.lockState = toggle ? CursorLockMode.None : CursorLockMode.Locked;
+        CanLook = !toggle;
+    }
+}

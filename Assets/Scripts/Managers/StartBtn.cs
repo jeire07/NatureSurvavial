1fe4143d321bc3dc9ebf0f3c3932efab1b55cb2b
jeire07 <149458625+jeire07@users.noreply.github.com>
2023-12-28 23:15:41 +0900

    [fix] load main scene

--- Assets/Scripts/Managers/StartBtn.cs
+++ Assets/Scripts/Managers/StartBtn.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartBtn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadMainScene()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("MainScene");
     }
 }
